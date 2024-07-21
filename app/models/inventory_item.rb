3a8081b5d0fe1e7fde11db3f33d99bdcdc7f1cfe
Rafeeq Shaik <rafeeq.shaik@valuelabs.com>
2024-07-21 23:09:38 +0530

    added broadcast for destroy

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,4 +1,5 @@
 class InventoryItem < ApplicationRecord
 	after_create_commit { broadcast_append_to "inventory_channel" }
 	after_update_commit { broadcast_replace_to "inventory_channel" }
+	after_destroy_commit { broadcast_remove_to "inventory_channel" }
 end
